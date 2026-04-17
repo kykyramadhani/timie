e1c9a82640091d04a653d185e72b54fcbdba8c38
kyky <rizkyinsania26@gmail.com>
2026-04-17 16:36:38 +0800

    Initial Commit

---
+++ timie/Level.swift
@@ -0,0 +1,8 @@
+//
+//  Level.swift
+//  timie
+//
+//  Created by Kyky on 17/04/26.
+//
+
+import Foundation
---
+++ timie/Timer.swift
@@ -0,0 +1,8 @@
+//
+//  Timer.swift
+//  timie
+//
+//  Created by Kyky on 17/04/26.
+//
+
+import Foundation
