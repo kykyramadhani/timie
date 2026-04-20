24b11d4a53c2a8f3d893482028c7de69f094ae2c
kyky <rizkyinsania26@gmail.com>
2026-04-20 17:02:15 +0800

    ADDED SCROLLABLE VIEW YEAAAAYYYYYYYYY

---
+++ timie/CardCarousel.swift
@@ -0,0 +1,18 @@
+//
+//  CardCarousel.swift
+//  timie
+//
+//  Created by Kyky on 20/04/26.
+//
+
+import SwiftUI
+
+struct CardCarousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardCarousel()
+}
---
+++ timie/CardComponent.swift
@@ -0,0 +1,18 @@
+//
+//  CardComponent.swift
+//  timie
+//
+//  Created by Kyky on 20/04/26.
+//
+
+import SwiftUI
+
+struct CardComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardComponent()
+}
