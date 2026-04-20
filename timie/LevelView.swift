54d6f9133afef0cdce7527b0f4188bae0518c2e8
pelangiromadoni222 <pelangisavana@gmail.com>
2026-04-20 14:12:59 +0800

    added LevelView, asset, accentcolor

---
+++ timie/LevelView.swift
@@ -0,0 +1,119 @@
+//
+//  LevelView.swift
+//  timie
+//
+//  Created by Pelangi Romadoni on 20/04/26.
+//
+
+
+
+//
+//  Untitled.swift
+//  timie
+//
+//  Created by Pelangi Romadoni on 20/04/26.
+//
+
+//
+//  ContentView.swift
+//  timie
+//
+//  Created by Pelangi Romadoni on 19/04/26.
+//
+
+import SwiftUI
+
+struct LevelView: View {
+    let bgColor = Color(red: 249/255, green: 242/255, blue: 208/255)
+    let headingColor = Color(red: 180/255, green: 59/255, blue: 6/255)
+    let subtitleColor = Color(red: 75/255, green: 69/255, blue: 66/255)
+    let mainShapeColor = Color(red: 102/255, green: 39/255, blue: 9/255)
+    let secondShapeColor = Color(red: 186/255, green: 57/255, blue: 1/255)
+    let buttonColor = Color(red: 73/255, green: 106/255, blue: 178/255)
+
+    var body: some View {
+        ZStack{
+            bgColor.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("timie")
+                    .font(Font.system(size: 20, weight: .bold, design: .default))
+                    .padding(25)
+                Text("How do you like your noodles?")
+                    .font(Font.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(headingColor)
+                    .padding(8)
+                Text("Choose a level.")
+                    .font(Font.system(size: 22, weight: .regular, design: .default))
+                    .foregroundColor(subtitleColor)
+                Spacer(minLength: 20)
+                
+                
+                ZStack(alignment: .bottomTrailing){
+                    RoundedRectangle(cornerRadius: 25)
+                        .foregroundColor(mainShapeColor)
+                        .frame(width: 264.58, height: 418)
+                    
+                    //Footer thing
+                    ZStack{
+                        UnevenRoundedRectangle(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 25,
+                            bottomTrailingRadius: 25,
+                            topTrailingRadius: 0
+                        )
+                        .foregroundColor(secondShapeColor)
+                        
+                        VStack(alignment: .leading, spacing: 20){
+                            HStack {
+                                Text("Perfect")
+                                    .font(Font.system(size: 22, weight: .bold, design: .default))
+                                    .padding(.horizontal, 20)
+                                    .foregroundColor(.white)
+                                Spacer()
+                                Label("3 mins", systemImage: "clock.fill")
+                                    .font(Font.system(size: 18, weight: .semibold, design: .default))
+                                    .padding(.horizontal, 20)
+                                    .foregroundColor(.white)
+                            }
+                            Text("Perfectly cooked. Chewy. As it's intended to eat.")
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.white)
+                        }
+                        
+                    }
+                    .frame(width: 264.58, height: 170)
+                    
+                }
+                Spacer()
+                NavigationLink {
+                    Text("placeholder")
+                } label: {
+                    Label{
+                        Text("Start Cooking")
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    } icon:{
+                        Image("ChefIcon")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding(.vertical, 20) //best practice is 44x44?
+                    .padding(.horizontal, 50)
+                    .background(Color.accentColor)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+                    .padding(20)
+                    //is there a better way to organize content? i feel like im just putting padding everywhere
+                } //changes here
+                
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    LevelView()
+}
+
+
