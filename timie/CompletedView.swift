5c80fa254ef229b1d006653933ae72d3860a5fc3
kyky <rizkyinsania26@gmail.com>
2026-04-21 15:27:57 +0800

    deleted infinite scroll and managed to make it non infinite

---
+++ timie/CompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedView.swift
+//  timie
+//
+//  Created by Kyky on 21/04/26.
+//
+
+import SwiftUI
+
+struct CompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedView()
+}
